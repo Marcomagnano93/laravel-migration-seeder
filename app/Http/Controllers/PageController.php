813984eb00892577f22cd1679e5d245482ab3d2c
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-24 16:39:32 +0200

    ADD: filtered trains in view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+
+        // $today = Carbon::now();
+        
+        $trains = Train::where('departure_time', '<', '2024-05-25 00:00:00')->get();
         return view ('home', compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,37 @@
     <div class="container">
         <h1>Trains</h1>
     </div>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#Codice</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Partenza</th>
+                <th scope="col">Destinazione</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">N Carrozze</th>
+                <th scope="col">In orario</th>
+                <th scope="col">Cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                <th scope="row">{{ $train->train_code }}</th>
+                <td>{{ $train->company }}</td>
+                <td>{{ $train->departure }}</td>
+                <td>{{ $train->destination }}</td>
+                <td>{{ $train->departure_time }}</td>
+                <td>{{ $train->arrival_time }}</td>
+                <td>{{ $train->train_carriages }}</td>
+                <td>
+                </td>
+                <td></td>
+                </tr>
+            @endforeach   
+            </tbody>
+        </table>
+    </div>
 @endsection
